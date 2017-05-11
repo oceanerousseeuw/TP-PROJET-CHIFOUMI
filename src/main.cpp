db59cf9982d80420d52749d97e3544bca8353bbc
oceane R <oceane.rousseeuw@gmail.com>
2017-05-11 15:23:28 +0200

    jeu

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <glog/logging.h>
+#include "game.hpp"
 
-int main(int argc, char ** argv) {
+int main() {
     google::InitGoogleLogging("log_chifoumi");
     google::SetLogDestination(google::GLOG_INFO, "log_chifoumi");
     std::string texte = "Hello world !";
     std::cout << texte << std::endl;
     LOG(INFO) << "test" ;
+
+    std::string move;
+    std::cout << "move (pierre, feuille, ciseaux, quit):";
+    std::cin >> move;
+    game g;
+    if (move == "quit"){
+    std::cout << "Au revoir !" << std::endl;
+    }else{
+    g.play(move);
+    }
+    
     return 0;
 }
 
